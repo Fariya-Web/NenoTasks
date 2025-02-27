6dc131cdd5c0c21d644256ec0b19e9fc8b462fc1
Fariya-Web <fariya.web@gmail.com>
2025-02-27 23:25:23 +0600

    style: community section icons

--- src/Components/HomeComp.jsx/Community.jsx
+++ src/Components/HomeComp.jsx/Community.jsx
@@ -11,30 +11,30 @@ const Community = () => {
             </p>
 
             <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-4 mt-10'>
-                {/* Forum */}
+               
                 <Link to='/forum' className='p-6 border rounded-2xl shadow-lg hover:shadow-xl transition flex flex-col items-center'>
-                    <FaUsers className='text-5xl text-blue-500' />
+                    <FaUsers className='text-5xl text-blue-300' />
                     <h3 className='text-xl font-semibold mt-4'>Discussion Forum</h3>
                     <p className='text-gray-600 mt-2'>Ask questions and share tips with the community.</p>
                 </Link>
 
-                {/* Blog */}
+               
                 <Link to='/blog' className='p-6 border rounded-2xl shadow-lg hover:shadow-xl transition flex flex-col items-center'>
-                    <FaBookOpen className='text-5xl text-green-500' />
+                    <FaBookOpen className='text-5xl text-green-300' />
                     <h3 className='text-xl font-semibold mt-4'>Blog & Guides</h3>
                     <p className='text-gray-600 mt-2'>Read success stories and expert tips.</p>
                 </Link>
 
-                {/* Help Center */}
+                
                 <Link to='/help-center' className='p-6 border rounded-2xl shadow-lg hover:shadow-xl transition flex flex-col items-center'>
-                    <FaQuestionCircle className='text-5xl text-yellow-500' />
+                    <FaQuestionCircle className='text-5xl text-yellow-200' />
                     <h3 className='text-xl font-semibold mt-4'>Help Center</h3>
                     <p className='text-gray-600 mt-2'>Find answers to common questions.</p>
                 </Link>
 
-                {/* Support */}
+               
                 <Link to='/support' className='p-6 border rounded-2xl shadow-lg hover:shadow-xl transition flex flex-col items-center'>
-                    <FaHeadset className='text-5xl text-red-500' />
+                    <FaHeadset className='text-5xl text-red-300' />
                     <h3 className='text-xl font-semibold mt-4'>Customer Support</h3>
                     <p className='text-gray-600 mt-2'>Need help? Reach out to our support team.</p>
                 </Link>
